76520a126905fee9db94fa0f017dcb9f86758767
Joy210 <sohanoor404@gmail.com>
2025-05-28 11:04:55 +0600

    update

--- app/components/IconsTabView.jsx
+++ app/components/IconsTabView.jsx
@@ -64,7 +64,7 @@ export default function IconsTabView({ iconsByCategory }) {
       </div>
 
       <div className="flex flex-col w-full h-full overflow-y-auto" ref={topRef}>
-        <div className="stick top-0 z-10 bg-gradient-to-b from-white to-90% px-5 py-4">
+        <div className="sticky top-0 z-10 bg-gradient-to-b from-white to-90% px-5 py-4">
           <SearchIcon
             searchVlaue={search}
             iconLength={filteredIcons.length}
